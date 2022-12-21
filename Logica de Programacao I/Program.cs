ac13c39c2c9efd7ca849e9e2c96404b6acb0f7ce
Rodrigo <rodrigo.freire@msn.com>
2022-12-21 12:22:52 -0300

    Primeiro commit + .gitignore

---
+++ Logica de Programacao I/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Logica_de_Programacao_I
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
